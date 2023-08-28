d629fb0f166a99d63d142d2e6406fc24db32390b
Lucas Carvalho <drigerm@hotmail.com>
2023-08-28 15:09:01 -0300

    add rota login check

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,47 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from 'cors';
 
 const prisma = new PrismaClient();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'lucasfullstacktest.up.railway.app'], // Adicione os URLs do frontend aqui
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Especifique os métodos permitidos
+  })
+);
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-  res.json(todos);
-});
+  try {
+    // Busque o usuário pelo email
+    const user = await prisma.user.findFirst({
+      where: { username: username },
+    });
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
-    data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
-    },
-  });
+    if (!user) {
+      // Usuário não encontrado
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
 
-  return res.json(todo);
-});
+    if (user.password !== password) {
+      // Senha incorreta
+      return res.status(401).json({ error: "Senha incorreta" });
+    }
 
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
+    // Login bem-sucedido
+    return res.json({ message: "Login bem-sucedido" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro interno do servidor" });
+  }
 });
 
 app.get("/", async (req, res) => {
